218f0c0453008ee29c20f87b76c066f6d9f61fd9
aleksandar popovic <popovicsail@gmail.com>
2025-05-19 17:28:22 +0200

    model initialized

---
+++ drustvena_mreza/Models/Grupa.cs
@@ -0,0 +1,17 @@
+﻿namespace drustvena_mreza.Models
+{
+    public class Grupa
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public DateTime DatumOsnivanja { get; set; }
+
+        public Grupa(int id, string ime, DateTime datumOsnivanja)
+        {
+            Id = id;
+            Ime = ime;
+            DatumOsnivanja = datumOsnivanja;
+        }
+
+    }
+}
---
+++ drustvena_mreza/Models/Korisnik.cs
@@ -0,0 +1,19 @@
+﻿namespace drustvena_mreza.Models
+{
+    public class Korisnik
+    {
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public DateTime DatumRodjenja { get; set; }
+
+        public Korisnik(int id, string ime, string prezime, DateTime datumRodjenja)
+        {
+            Id = id;
+            Ime = ime;
+            Prezime = prezime;
+            DatumRodjenja = datumRodjenja;
+        }
+
+    }
+}
